18b0484cafa62857c26f23bd5e601fb92089b307
Alan Yeung <yeungalan0@gmail.com>
2021-02-13 14:48:30 -0700

    Cleaned up

--- pages/couple-game.tsx
+++ pages/couple-game.tsx
@@ -1,10 +1,55 @@
 import styles from '../styles/Home.module.css'
-import React, { useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useRef, useState } from 'react'
 import * as constants from '../constants/couple-game'
 
 export default function CoupleGame() {
+  const [players, setPlayers] = useState<string[]>([])
+  const playerInput = useRef<HTMLInputElement>(null)
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    setPlayers(playerInput.current!.value.split(","))
+    event.preventDefault()
+  }
+
+  let content: JSX.Element
+
+  if (players.length > 0) {
+    console.log(`PLAYERS: ${players}, ${players.length}`)
+    content =  <ActualCoupleGame players={players}/>
+  } else {
+    content = (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label title="Enter comma separated names, e.g. 'Alan,Jen'">
+            Input player names:
+            <input type="text" ref={playerInput}/>
+          </label>
+          <input type="submit" value="Let's play!" />
+        </form>
+      </div>
+    )
+  }
+
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>Welcome to the couple game! 💕😘</h1>
+        <br />
+        {content}
+      </main>
+      <footer>
+        <p>Dedicated to my wonderful girlfriend, Jen 😳</p>
+      </footer>
+    </div>
+  )
+}
+
+type CoupleGameProps = {
+  players: string[]
+}
+
+function ActualCoupleGame({ players }: CoupleGameProps) {
   const [card, setCard] = useState(new Card("Game hasn't started", "Are you ready to play?"))
-  const [players, setPlayers] = useState([""])
   const [turn, setTurn] = useState(0)
 
   useEffect(() => {
@@ -13,13 +58,13 @@ export default function CoupleGame() {
     }
   })
 
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    alert(`Players: ${players} (${players.length})`)
-    event.preventDefault()
-  }
-
-  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    setPlayers(event.target.value.split(","))
+  function init() {
+    const size = constants.GUESS_LIST.length + constants.CHALLENGE_LIST.length
+    const possibleIndexes = Array.from(Array(size).keys())
+  
+    localStorage.setItem(constants.INDEXES_KEY, JSON.stringify(possibleIndexes))
+  
+    setCard(getCard())
   }
 
   function nextTurn() {
@@ -28,52 +73,35 @@ export default function CoupleGame() {
   }
 
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1>Welcome to the couple game! 💕😘</h1>
-        <br />
-        <form onSubmit={handleSubmit}>
-          <label>
-            Player names:
-            <input type="text" value={players} onChange={handleChange}/>
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-        <p>
-          Player turn: {players[turn % players.length]}
-        </p>
-        <p>
-          Card type: {card.type} 😉🧐
-        </p>
-        <p>
-          {card.text}
-        </p>
-        <button onClick={() => nextTurn()}>
-          Next turn
-        </button>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <button onClick={() => init()}>
-          Reset
-        </button>
-      </main>
-    </div>
+    <Fragment>
+      <p>
+        Players: {players.join(", ")}
+      </p>
+      <p>
+        Player turn: {players[turn % players.length]}
+      </p>
+      <p>
+        Card type: {card.type} 😉🧐
+      </p>
+      <p>
+        {card.text}
+      </p>
+      <button onClick={() => nextTurn()}>
+        Next turn
+      </button>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <button onClick={() => init()}>
+        Reset
+      </button>
+    </Fragment>
   )
-}
-
-function init() {
-  const size = constants.GUESS_LIST.length + constants.CHALLENGE_LIST.length
-  const possibleIndexes = Array.from(Array(size).keys())
-
-  localStorage.setItem(constants.INDEXES_KEY, JSON.stringify(possibleIndexes))
-
-  window.location.reload()
 }
 
 function getCard(): Card {
