4156b0430df22564154ff4a583c5321da87143d3
Alan Yeung <yeungalan0@gmail.com>
2021-03-20 09:59:22 -0600

    Minor header updates, still need to style

--- src/header.tsx
+++ src/header.tsx
@@ -1,10 +1,25 @@
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import Link from "next/link";
 
 const Header = (): JSX.Element => {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography>TODO</Typography>
+        <Typography color="textPrimary" variant="h6">
+          <Link href={`/about`}>
+            <a>About</a>
+          </Link>
+        </Typography>
+        <Typography color="textPrimary" variant="h6">
+          <Link href={`/blog`}>
+            <a>Blog</a>
+          </Link>
+        </Typography>
+        <Typography color="textPrimary" variant="h6">
+          <Link href={`/blog`}>
+            <a>Resume</a>
+          </Link>
+        </Typography>
       </Toolbar>
     </AppBar>
   );
