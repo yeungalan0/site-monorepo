78d5caa9556b9c9701a25dba7b7489b8d3d3dec6
Alan Yeung <yeungalan0@gmail.com>
2022-02-20 12:01:54 -0700

    Initial commit

---
+++ life_in_weeks/README.md
@@ -0,0 +1,11 @@
+# Setup test environment
+
+```
+make test
+```
+
+# Clean up test environment (should be run automatically on CTRL-C of `make test`)
+
+```
+make clean
+```
---
+++ life_in_weeks/main.ts
@@ -0,0 +1,60 @@
+import { Construct } from "constructs";
+import { App, TerraformStack } from "cdktf";
+import { AwsProvider, dynamodb } from "./.gen/providers/aws";
+
+class MyStack extends TerraformStack {
+  constructor(scope: Construct, name: string) {
+    super(scope, name);
+
+    new AwsProvider(this, "aws", {
+      region: "us-east-1",
+      endpoints: [
+        {
+          dynamodb: "http://localhost:4566",
+          sts: "http://localhost:4566",
+        },
+      ],
+    });
+
+    new dynamodb.DynamodbTable(this, `NextAuthTable`, {
+      name: "next-auth",
+      hashKey: "pk",
+      rangeKey: "sk",
+      ttl: { enabled: true, attributeName: "expires" },
+      readCapacity: 10, // TODO
+      writeCapacity: 10,
+      globalSecondaryIndex: [
+        {
+          name: "GSI1",
+          hashKey: "GSI1PK",
+          rangeKey: "GSI1SK",
+          projectionType: "ALL",
+          readCapacity: 10,
+          writeCapacity: 10,
+        },
+      ],
+      attribute: [
+        {
+          name: "pk",
+          type: "S",
+        },
+        {
+          name: "sk",
+          type: "S",
+        },
+        {
+          name: "GSI1PK",
+          type: "S",
+        },
+        {
+          name: "GSI1SK",
+          type: "S",
+        },
+      ],
+    });
+  }
+}
+
+const app = new App();
+new MyStack(app, "life_in_weeks");
+app.synth();
