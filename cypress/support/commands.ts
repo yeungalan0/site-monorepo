0e33d4dab5cff2f2274408835e8b6b4b5d01abad
Alan Yeung <yeungalan0@gmail.com>
2022-02-21 13:46:30 -0700

    Minimum updates to Cypress tests to test login. More to do

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,3 +1,4 @@
+/// <reference path="../support/index.d.ts" />
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -23,3 +24,19 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("login", () => {
+  cy.intercept("/api/auth/session", { fixture: "session.json" }).as("session");
+
+  // Set the cookie for cypress.
+  // It has to be a valid cookie so next-auth can decrypt it and confirm its validity.
+  // This step can probably/hopefully be improved.
+  // We are currently unsure about this part.
+  // We need to refresh this cookie once in a while.
+  // We are unsure if this is true and if true, when it needs to be refreshed.
+  cy.setCookie(
+    "next-auth.session-token",
+    "a valid cookie from your browser session"
+  );
+  Cypress.Cookies.preserveOnce("next-auth.session-token");
+});
---
+++ cypress/support/index.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+declare namespace Cypress {
+  interface Chainable {
+    /**
+     * Custom command to select set session and mock next-auth login during testing
+     * @example cy.login()
+     */
+    login(): void;
+  }
+}
