4ea9b50f0574c95287aeb9d4c707211403e28c67
Alan Yeung <yeungalan0@gmail.com>
2022-02-27 10:11:37 -0700

    Updates pipeline to setup localstack

--- infra/life_in_weeks/main.ts
+++ infra/life_in_weeks/main.ts
@@ -10,8 +10,9 @@ class MyStack extends TerraformStack {
       region: "us-east-1",
       endpoints: [
         {
-          dynamodb: "http://localhost:4566",
-          sts: "http://localhost:4566",
+          dynamodb:
+            process.env.NEXT_AUTH_DYNAMO_ENDPOINT ?? "http://localhost:4566",
+          sts: process.env.NEXT_AUTH_STS_ENDPOINT ?? "http://localhost:4566",
         },
       ],
     });
